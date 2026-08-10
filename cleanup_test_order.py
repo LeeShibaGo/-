3ed6a719021dda55683e5f689b5d84f08995863f
ShibaGo <journeylee321@gmail.com>
2026-08-10 14:12:38 +0800

    Repoint cleanup script at QATEST2 vulnerability-test junk order

--- cleanup_test_order.py
+++ cleanup_test_order.py
@@ -17,7 +17,7 @@ for _s in (sys.stdout, sys.stderr):
 
 FIREBASE_DB_URL = "https://shibago-4dd3c-default-rtdb.asia-southeast1.firebasedatabase.app"
 ORDERS_PATH = "daigou-orders-v1"
-BAD_KEY = "QATEST1"
+BAD_KEY = "QATEST2"
 
 
 def main():
